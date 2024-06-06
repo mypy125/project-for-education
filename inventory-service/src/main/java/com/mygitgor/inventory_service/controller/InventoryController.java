020beb3a258a5900e2df14a8a1ecbbaf435a935e
mohito <mypywelcome@gmail.com>
2024-06-06 21:03:16 +0400

    add_project_education_L2

---
+++ inventory-service/src/main/java/com/mygitgor/inventory_service/controller/InventoryController.java
@@ -0,0 +1,26 @@
+package com.mygitgor.inventory_service.controller;
+
+import com.mygitgor.inventory_service.dto.InventoryResponse;
+import com.mygitgor.inventory_service.service.InventoryService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/inventory")
+public class InventoryController {
+
+    private final InventoryService inventoryService;
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<InventoryResponse>isInStock(@RequestParam List<String> skuCode){
+        log.info("Received inventory check request for skuCode: {}", skuCode);
+        return inventoryService.isInStock(skuCode);
+    }
+}
---
+++ inventory-service/src/main/java/com/mygitgor/inventory_service/repository/InventoryRepository.java
@@ -0,0 +1,12 @@
+package com.mygitgor.inventory_service.repository;
+
+import com.mygitgor.inventory_service.model.Inventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+    List<Inventory> findBySkuCodeIn(List<String> skuCode);
+}
