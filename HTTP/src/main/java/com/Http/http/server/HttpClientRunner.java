64fc28b8f8525d2ed29d07cedb84f47190270baf
mohito <mypywelcome@gmail.com>
2024-06-29 16:05:30 +0400

    add_for_edu_HTTP request asynchronously

--- HTTP/src/main/java/com/Http/http/server/HttpClientRunner.java
+++ HTTP/src/main/java/com/Http/http/server/HttpClientRunner.java
@@ -6,11 +6,12 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.nio.file.Path;
+import java.util.concurrent.ExecutionException;
 
 import static java.net.http.HttpRequest.BodyPublishers.ofFile;
 
 public class HttpClientRunner {
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args) throws IOException, InterruptedException, ExecutionException {
         var httpClient = HttpClient.newBuilder()
                 .version(HttpClient.Version.HTTP_1_1)
                 .build();
@@ -18,11 +19,19 @@ public class HttpClientRunner {
         var request = HttpRequest.newBuilder()
                 .uri(URI.create("http://localhost:9000"))
                 .header("content-type", "application/json")
-                .POST(ofFile(Path.of("resources","first.json")))
+                .POST(ofFile(Path.of("resources", "first.json")))
                 .build();
 
+        //Метод send в HttpClient используется для отправки HTTP-запроса синхронно.
         var response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
         System.out.println(response.headers());
         System.out.println(response.body());
+
+        //Метод sendAsync в HttpClient используется для отправки HTTP-запроса асинхронно.
+        var response1 = httpClient.sendAsync(request, HttpResponse.BodyHandlers.ofString());
+        var response2 = httpClient.sendAsync(request, HttpResponse.BodyHandlers.ofString());
+        var response3 = httpClient.sendAsync(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response3.get().body());
+
     }
 }
--- HTTP/src/main/java/com/Http/http/server/HttpServer.java
+++ HTTP/src/main/java/com/Http/http/server/HttpServer.java
@@ -7,19 +7,33 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
+/**
+ * ExecutorService в Java используется для управления пулом потоков,
+ * что позволяет вам выполнять асинхронные задачи более эффективно,
+ * контролируя количество одновременно работающих потоков.
+ * Объявление private final ExecutorService pool в классе означает,
+ * что этот класс будет использовать пул потоков для выполнения задач.
+ */
 public class HttpServer {
+    private final ExecutorService pool;
     private final int port;
+    private boolean stopped;
 
-    public HttpServer(int port) {
+    public HttpServer(int port, int poolSize) {
         this.port = port;
+        this.pool = Executors.newFixedThreadPool(poolSize);
     }
 
     public void run(){
         try {
-            var serverSocket = new ServerSocket(port);
-            var accept = serverSocket.accept();
-            processSocket(accept);
+            var server = new ServerSocket(port);
+            while (!stopped){
+                var socket = server.accept();
+                pool.submit(()-> processSocket(socket));
+            }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -33,6 +47,7 @@ public class HttpServer {
 
             System.out.println("Request: " + new String(inputStream.readNBytes(400)));
 
+            Thread.sleep(10000);
             var body = Files.readAllBytes(Path.of("resources", "first.html"));
 
             var headers = """
@@ -43,9 +58,13 @@ public class HttpServer {
             outputStream.write(headers);
             outputStream.write(System.lineSeparator().getBytes());
             outputStream.write(body);
-        } catch (IOException e) {
+        } catch (IOException | InterruptedException e) {
             e.printStackTrace();
         }
+    }
+
+    public void setStopped(boolean stopped){
+        this.stopped = stopped;
     }
 
 
